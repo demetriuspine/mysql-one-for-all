aac8b6bff55a89f590ed0f208f2dc03dc4bc5e07
lucas <mr.pine97@gmail.com>
2022-01-08 14:24:44 -0300

    feat: desafio 3 COMPLETO: cria uma query que exibe o total de musicas e minutos por usuario

---
+++ desafio3.sql
@@ -0,0 +1,11 @@
+SELECT
+	u.NOME_USUARIO AS usuario,
+	COUNT(m.ID_USUARIO) AS qtde_musicas_ouvidas,
+	ROUND(SUM(c.DURACAO_CANCAO_SEGUNDOS / 60), 2) AS total_minutos -- http://www.bosontreinamentos.com.br/mysql/arredondar-valores-em-consultas-com-funcao-round-no-mysql/
+FROM SpotifyClone.usuarios AS u
+INNER JOIN SpotifyClone.musicas_reproduzidas AS m
+ON u.ID_USUARIO = m.ID_USUARIO
+INNER JOIN SpotifyClone.cancoes as c
+ON m.ID_CANCAO = c.ID_CANCAO
+GROUP BY u.NOME_USUARIO
+ORDER BY u.NOME_USUARIO ASC;
