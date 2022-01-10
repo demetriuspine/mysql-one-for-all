701cdf4aa9f2f1ce8844a94c92307fcf16d179c2
lucas <mr.pine97@gmail.com>
2022-01-10 12:18:21 -0300

    feat: desafio 8 COMPLETO: cria uma query que exibe todos o albuns do artista Walter Phoenix

---
+++ desafio8.sql
@@ -0,0 +1,7 @@
+SELECT
+  a.NOME_ARTISTA AS artista,
+  al.NOME_ALBUM AS album
+FROM SpotifyClone.artistas AS a
+INNER JOIN SpotifyClone.albuns AS al
+ON a.ID_ARTISTA = al.ID_ARTISTA
+WHERE a.NOME_ARTISTA = 'Walter Phoenix';
